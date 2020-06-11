7dffc06a2d9c8f452749e8bb57bed9a833b462ee
PGPetkov18 <56921557+PGPetkov18@users.noreply.github.com>
2020-06-11 19:48:29 +0300

    Replace code in external files

---
+++ strings/strings/strings.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <cmath>
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+    int ch;
+    string q15, q15A, q15B, q15C, q15D;
+    do
+    {
+        ch = displayMenu();
+        choice1(ch, q15, q15A, q15B, q15C, q15D);
+
+    } while (1 > 0);
+}
