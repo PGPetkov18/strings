ef4fc2b20b03cde80d4f1bd9e00cb7e963c8fafc
Петър Петков <PGPetkov18@codingburgas.bg>
2020-06-11 19:43:06 +0300

    Replace code to external files

---
+++ strings/Header.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void gameGreetings();
+char displayMenu();
+void q1(string q1, string q1A, string q1B, string q1C, string q1D);
+void q2(string q2, string q2A, string q2B, string q2C, string q2D);
+void q3(string q3, string q3A, string q3B, string q3C, string q3D);
+void q4(string q4, string q4A, string q4B, string q4C, string q4D);
+void q6(string q6, string q6A, string q6B, string q6C, string q6D);
+void q7(string q7, string q7A, string q7B, string q7C, string q7D);
+void q8(string q8, string q8A, string q8B, string q8C, string q8D);
+void q9(string q9, string q9A, string q9B, string q9C, string q9D);
+void q11(string q11, string q11A, string q11B, string q11C, string q11D);
+void q12(string q12, string q12A, string q12B, string q12C, string q12D);
+void q13(string q13, string q13A, string q13B, string q13C, string q13D);
+void q14(string q14, string q14A, string q14B, string q14C, string q14D);
+void q15(string q15, string q15A, string q15B, string q15C, string q15D);
+void q10(string q10, string q10A, string q10B, string q10C, string q10D);
+void q5(string q5, string q5A, string q5B, string q5C, string q5D);
+void choice1(int& choice, string& q15, string& q15A, string& q15B, string& q15C, string& q15D);
+
